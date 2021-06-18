d445d30e902f70d83d179a36e0bea271eacf06c8
Rémy Baranx (bar) <bar@odoo.com>
2021-06-18 17:45:45 +0200

    change admin URL

--- eroo/urls.py
+++ eroo/urls.py
@@ -7,7 +7,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("yaap/", admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path("accounts/", include("allauth.urls")),
     path("websites/", include("websites.urls")),
